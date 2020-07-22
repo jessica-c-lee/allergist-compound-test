d7fc51903aea9188ef66e52695d895e1f8b13697
jessica-c-lee <jesssica.lee88@gmail.com>
2020-07-22 14:26:59 +1000

    Add experiment files, working version ready for SONA

---
+++ NSW52-program/jspsych-6.1.0/custom/jspsych-compound-forced-choice.js
@@ -0,0 +1,274 @@
+/**
+ * jspsych-compound-forced-choice
+ * Jessica C. Lee
+ *
+ * plugin to display two compound cues and record keypress response
+ *
+ **/
+
+
+jsPsych.plugins["compound-forced-choice"] = (function() {
+
+  var plugin = {};
+
+  jsPsych.pluginAPI.registerPreload('compound-forced-choice', 'stimulus', 'image');
+
+  plugin.info = {
+    name: 'compound-forced-choice',
+    description: '',
+    parameters: {
+      stimulus1: {
+        type: jsPsych.plugins.parameterType.IMAGE,
+        pretty_name: 'Stimulus 1',
+        default: undefined,
+        description: 'The first image to be displayed'
+      },
+      stimulus2: {
+        type: jsPsych.plugins.parameterType.IMAGE,
+        pretty_name: 'Stimulus 2',
+        default: undefined,
+        description: 'The second image to be displayed'
+      },
+      stimulus3: {
+        type: jsPsych.plugins.parameterType.IMAGE,
+        pretty_name: 'Stimulus 3',
+        default: undefined,
+        description: 'The third image to be displayed'
+      },
+      stimulus4: {
+        type: jsPsych.plugins.parameterType.IMAGE,
+        pretty_name: 'Stimulus 4',
+        default: undefined,
+        description: 'The fourth image to be displayed'
+      },
+      stimulus5: {
+        type: jsPsych.plugins.parameterType.IMAGE,
+        pretty_name: 'Stimulus 5',
+        default: undefined,
+        description: 'The fifth image to be displayed'
+      },
+      stimulus_height: {
+        type: jsPsych.plugins.parameterType.INT,
+        pretty_name: 'Image height',
+        default: null,
+        description: 'Set the image height in pixels'
+      },
+      stimulus_width: {
+        type: jsPsych.plugins.parameterType.INT,
+        pretty_name: 'Image width',
+        default: null,
+        description: 'Set the image width in pixels'
+      },
+      maintain_aspect_ratio: {
+        type: jsPsych.plugins.parameterType.BOOL,
+        pretty_name: 'Maintain aspect ratio',
+        default: true,
+        description: 'Maintain the aspect ratio after setting width or height'
+      },
+      choices: {
+        type: jsPsych.plugins.parameterType.KEYCODE,
+        array: true,
+        pretty_name: 'Choices',
+        default: jsPsych.ALL_KEYS,
+        description: 'The keys the subject is allowed to press to respond to the stimulus.'
+      },
+      prompt: {
+        type: jsPsych.plugins.parameterType.STRING,
+        pretty_name: 'Prompt',
+        default: null,
+        description: 'Any content here will be displayed below the stimulus.'
+      },
+      stimulus_duration: {
+        type: jsPsych.plugins.parameterType.INT,
+        pretty_name: 'Stimulus duration',
+        default: null,
+        description: 'How long to hide the stimulus.'
+      },
+      trial_duration: {
+        type: jsPsych.plugins.parameterType.INT,
+        pretty_name: 'Trial duration',
+        default: null,
+        description: 'How long to show trial before it ends.'
+      },
+      response_ends_trial: {
+        type: jsPsych.plugins.parameterType.BOOL,
+        pretty_name: 'Response ends trial',
+        default: true,
+        description: 'If true, trial will end when subject makes a response.'
+      },
+    }
+  }
+
+  plugin.trial = function(display_element, trial) {
+
+    // display stimulus 1 ------------------------------------------------------
+    var html = '<img src="'+trial.stimulus1+'" id="jspsych-compound-forced-choice-stimulus" style="';
+    if(trial.stimulus_height !== null){
+      html += 'height:'+trial.stimulus_height+'px; '
+      if(trial.stimulus_width == null && trial.maintain_aspect_ratio){
+        html += 'width: auto; ';
+      }
+    }
+    if(trial.stimulus_width !== null){
+      html += 'width:'+trial.stimulus_width+'px; '
+      if(trial.stimulus_height == null && trial.maintain_aspect_ratio){
+        html += 'height: auto; ';
+      }
+    }
+    html +='"></img>';
+
+    // display stimulus 2 ------------------------------------------------------
+    html += '<img src="'+trial.stimulus2+'" id="jspsych-compound-forced-choice-stimulus" style="';
+    if(trial.stimulus_height !== null){
+      html += 'height:'+trial.stimulus_height+'px; '
+      if(trial.stimulus_width == null && trial.maintain_aspect_ratio){
+        html += 'width: auto; ';
+      }
+    }
+    if(trial.stimulus_width !== null){
+      html += 'width:'+trial.stimulus_width+'px; '
+      if(trial.stimulus_height == null && trial.maintain_aspect_ratio){
+        html += 'height: auto; ';
+      }
+    }
+    html +='"></img>';
+
+    // display stimulus 3 ------------------------------------------------------
+    html += '<img src="'+trial.stimulus3+'" id="jspsych-compound-forced-choice-stimulus" style="';
+    if(trial.stimulus_height !== null){
+      html += 'height:'+trial.stimulus_height+'px; '
+      if(trial.stimulus_width == null && trial.maintain_aspect_ratio){
+        html += 'width: auto; ';
+      }
+    }
+    if(trial.stimulus_width !== null){
+      html += 'width:'+trial.stimulus_width+'px; '
+      if(trial.stimulus_height == null && trial.maintain_aspect_ratio){
+        html += 'height: auto; ';
+      }
+    }
+    html +='"></img>';
+
+    // display stimulus 4 ------------------------------------------------------
+    html += '<img src="'+trial.stimulus4+'" id="jspsych-compound-forced-choice-stimulus" style="';
+    if(trial.stimulus_height !== null){
+      html += 'height:'+trial.stimulus_height+'px; '
+      if(trial.stimulus_width == null && trial.maintain_aspect_ratio){
+        html += 'width: auto; ';
+      }
+    }
+    if(trial.stimulus_width !== null){
+      html += 'width:'+trial.stimulus_width+'px; '
+      if(trial.stimulus_height == null && trial.maintain_aspect_ratio){
+        html += 'height: auto; ';
+      }
+    }
+    html +='"></img>';
+
+    // display stimulus 5 ------------------------------------------------------
+    html += '<img src="'+trial.stimulus5+'" id="jspsych-compound-forced-choice-stimulus" style="';
+    if(trial.stimulus_height !== null){
+      html += 'height:'+trial.stimulus_height+'px; '
+      if(trial.stimulus_width == null && trial.maintain_aspect_ratio){
+        html += 'width: auto; ';
+      }
+    }
+    if(trial.stimulus_width !== null){
+      html += 'width:'+trial.stimulus_width+'px; '
+      if(trial.stimulus_height == null && trial.maintain_aspect_ratio){
+        html += 'height: auto; ';
+      }
+    }
+    html +='"></img>';
+
+    //--------------------------------------------------------------------------
+
+    // add prompt
+    if (trial.prompt !== null){
+      html += trial.prompt;
+    }
+
+    // render
+    display_element.innerHTML = html;
+
+    // store response
+    var response = {
+      rt: null,
+      key: null
+    };
+
+    // function to end trial when it is time
+    var end_trial = function() {
+
+      // kill any remaining setTimeout handlers
+      jsPsych.pluginAPI.clearAllTimeouts();
+
+      // kill keyboard listeners
+      if (typeof keyboardListener !== 'undefined') {
+        jsPsych.pluginAPI.cancelKeyboardResponse(keyboardListener);
+      }
+
+      // gather the data to store for the trial
+      var trial_data = {
+        "rt": response.rt,
+        "stimulus1": trial.stimulus1,
+        "stimulus2": trial.stimulus2,
+        "stimulus3": trial.stimulus3,
+        "stimulus4": trial.stimulus4,
+        "stimulus5": trial.stimulus5,
+        "key_press": response.key
+      };
+
+      // clear the display
+      display_element.innerHTML = '';
+
+      // move on to the next trial
+      jsPsych.finishTrial(trial_data);
+    };
+
+    // function to handle responses by the subject
+    var after_response = function(info) {
+
+      // after a valid response, the stimulus will have the CSS class 'responded'
+      // which can be used to provide visual feedback that a response was recorded
+      display_element.querySelector('#jspsych-compound-forced-choice-stimulus').className += ' responded';
+
+      // only record the first response
+      if (response.key == null) {
+        response = info;
+      }
+
+      if (trial.response_ends_trial) {
+        end_trial();
+      }
+    };
+
+    // start the response listener
+    if (trial.choices != jsPsych.NO_KEYS) {
+      var keyboardListener = jsPsych.pluginAPI.getKeyboardResponse({
+        callback_function: after_response,
+        valid_responses: trial.choices,
+        rt_method: 'performance',
+        persist: false,
+        allow_held_key: false
+      });
+    }
+
+    // hide stimulus if stimulus_duration is set
+    if (trial.stimulus_duration !== null) {
+      jsPsych.pluginAPI.setTimeout(function() {
+        display_element.querySelector('#jspsych-compound-forced-choice-stimulus').style.visibility = 'hidden';
+      }, trial.stimulus_duration);
+    }
+
+    // end trial if trial_duration is set
+    if (trial.trial_duration !== null) {
+      jsPsych.pluginAPI.setTimeout(function() {
+        end_trial();
+      }, trial.trial_duration);
+    }
+
+  };
+
+  return plugin;
+})();
